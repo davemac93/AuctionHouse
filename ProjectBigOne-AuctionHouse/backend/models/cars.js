22bbfa1fb8f476c72d220b9c9705d62469dd31ea
Dawid Maciejewski <dawid.mac@hotmail.com>
2024-04-17 23:41:02 +0200

    Added new features to add cars, add blog, update view from collection and blog, add blogs details page, update user profile view

---
+++ ProjectBigOne-AuctionHouse/backend/models/cars.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const carSchema = new Schema({
+    model: String,
+    make: String,
+    year: Number,
+    mileage: Number,
+    auctionEnd: Date,
+    startingPrice: Number,
+    currentAuctionPrice: { type: Number }, // Removed the default value
+    userID: { type: Schema.Types.ObjectId, ref: 'User' },
+    imagePaths: [{ type: String }],
+    detailsPage: String
+});
+
+// Initialize currentAuctionPrice with startingPrice
+carSchema.pre('save', function(next) {
+    if (!this.currentAuctionPrice) {
+        this.currentAuctionPrice = this.startingPrice;
+    }
+    next();
+});
+
+// Method to place a bid on the car auction
+carSchema.methods.placeBid = function(newPrice, userID) {
+    if (newPrice > this.currentAuctionPrice) {
+        this.currentAuctionPrice = newPrice;
+        this.userID = userID;
+        return true; // Bid successfully placed
+    } else {
+        return false; // Bid price is not higher than current auction price
+    }
+};
+
+// Method to check if the auction is still ongoing
+carSchema.methods.isAuctionOngoing = function() {
+    return Date.now() < this.auctionEnd;
+};
+
+const Car = mongoose.model('Car', carSchema);
+
+module.exports = Car;
